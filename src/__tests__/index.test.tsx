b8ab33920cd7a80473b34db36e1504d2d682a4b2
Adrian Rusnarczyk <adrian@signd.id>
2022-06-14 21:10:11 +0200

    Add unit tests

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,35 @@
-it.todo('write a test');
+import {
+  SigndResult,
+  VerificationResult,
+  ProgressBarStyle,
+  SigndInterface,
+} from '../';
+import { mock } from 'jest-mock-extended';
+
+test('SignD initialize function', async () => {
+  const mockedSigndModule = mock<SigndInterface>();
+  await mockedSigndModule.initialize({
+    scheme: 'signd',
+    host: 'session',
+    apiUrl: 'https://api.dev.signd.io',
+    progressBarStyle: ProgressBarStyle.Linear,
+  });
+
+  expect(mockedSigndModule.initialize).toBeCalledTimes(1);
+});
+
+test('SignD start function', async () => {
+  const mockedSigndModule = mock<SigndInterface>();
+  mockedSigndModule.start.mockReturnValue(
+    Promise.resolve({
+      result: VerificationResult.ProcessFinished,
+      sessionToken: '123456789',
+    })
+  );
+  const { result, sessionToken }: SigndResult = await mockedSigndModule.start(
+    '123456789'
+  );
+  expect(mockedSigndModule.start).toBeCalledTimes(1);
+  expect(result).toBe(VerificationResult.ProcessFinished);
+  expect(sessionToken).toBe('123456789');
+});
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import { NativeModules } from 'react-native';
-import { SigndOptions, SigndResult, VerificationResult } from './types';
+import {
+  ProgressBarStyle,
+  SigndOptions,
+  SigndResult,
+  VerificationResult,
+} from './types';
 const { SigndModule } = NativeModules;
 
-interface SigndInterface {
+export interface SigndInterface {
   initialize(config: SigndOptions): Promise<void>;
   start(sessionToken: string): Promise<SigndResult>;
 }
 
 export default SigndModule as SigndInterface;
 
-export { VerificationResult, SigndResult };
+export { VerificationResult, SigndResult, ProgressBarStyle };
